047b5e0ba6f09f0e3f6cc75f91e77fa70eff9fc8
StasButton <80902229+StasButton@users.noreply.github.com>
2022-11-13 19:00:11 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -44,6 +44,5 @@ result = st.button('Распознать изображение')
 if result:
     x = preprocess_image(img)
     preds = model.predict(x)
-    st.button('Распознать')
     st.write('**Результаты распознавания:**')
     print_predictions(preds)
