b347b22acf95551eba1067b90bee27850de0a53a
StasButton <80902229+StasButton@users.noreply.github.com>
2023-05-01 16:43:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,93 @@
 import io
 import streamlit as st
-from PIL import Image
+import pandas as pd
 import numpy as np
-
 '''
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+turniket = 0 
+def turnlevel(d):
+    ff = []; 
+    f1 = [1,2,3,5,8,9,10,11,12,13,15,16,17,18,19]
+    ff.append(f1)
+    f2 = [6,7,14]
+    ff.append(f2)
+    f3 = [20]
+    ff.append(f3)
+    f4 = [21,22,23,24,31,36,38,39,40,42]
+    ff.append(f4)
+    f5 = [25,26,27,29,30]
+    ff.append(f5)
+    f6 = [4]
+    ff.append(f6)
 
+    for i in range(len(ff)):
+        for i2 in ff[i]:
+            if i2 == d:
+              if i == 0:
+                turniket = 470
+              if i == 1:
+                turniket = 580
+              if i == 2:
+                turniket = 850
+              if i == 3:
+                turniket = 430
+              if i == 4:
+                turniket = 510
+              if i == 5:
+                turniket = 910
+    return  turniket
+        #-------------------------
+def load_vid():
+    t = st.text_input("Введите имя файла (без расширения) (1-43) и нажмите Enter")
+    www = st.text_input("html-link. Уровень турникета для видеороликов - 1,2,3,5,8,9,10,11,12,13,15,16,17,18,19")
+    
+    return t, www
+#==================================================================   
+st.title('YOLOv8_tracking людей, касок, жилетов')
+st.write('Загрузка видеоролика (из репозитория)')
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    return EfficientNetB0(weights='imagenet')
+t, www = load_vid()
+if www is not '':
+    st.write(www)
+    turniket = 470
+    st.video(www)
+    #st.video(vd)
+    yolo_weights = 'last_8n_e120.pt'
+    import yolov8_tracking.track as track
+    opt = track.parse_opt(yolo_weights,www)
+    fstr = track.main(opt)
+    if(len(fstr)) != 0:
+        import treatment as obr
+        df = obr.END(fstr,turniket)
+        st.write('Результат роботы нейросети')
 
+    st.write(df)
 
-def preprocess_image(img):
-    img = img.resize((224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
+if t is not '':
+    
+    d = int(t)
+    st.write(t+'.mp4')
+    turniket = turnlevel(d)
 
+    if (d <= 43):
 
-def load_image():
-    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
-    if uploaded_file is not None:
-        image_data = uploaded_file.getvalue()
-        st.image(image_data)
-        return Image.open(io.BytesIO(image_data))
-    else:
-        return None
-
-
-def print_predictions(preds):
-    classes = decode_predictions(preds, top=3)[0]
-    for cl in classes:
-        st.write(cl[1], cl[2])
-
-
-model = load_model()
-
-st.title('Классификации изображений в облаке Streamlit')
-img = load_image()
-
-result = st.button('Распознать изображение')
-if result:
-    x = preprocess_image(img)
-    preds = model.predict(x)
-    st.write('**Результаты распознавания:**')
-    print_predictions(preds)
+        t = str(t)
+        p = 'Videos/'+t+'.mp4'
+        vd = open(p,'rb')
+        st.video(vd)
+        yolo_weights = 'last_8n_e120.pt'
+        import yolov8_tracking.track as track
+        opt = track.parse_opt(yolo_weights,p)
+        fstr = track.main(opt)
+        
+        if(len(fstr)) != 0:
+            import treatment as obr
+            df = obr.END(fstr,turniket)
+            st.write('Результат роботы нейросети')
+            
+            st.write(df)
+        else :
+            df = pd.DataFrame(columns=['files','in','out','каски','жилеты'])
+            #st.write('Результат роботы нейросети')
+            df.loc[0] = [1,0,0,0,0]
+            st.write(df)
 '''
