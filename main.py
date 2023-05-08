95fe2361a5ce30f11215cfc5e83adb4d887fd4b5
StasButton <80902229+StasButton@users.noreply.github.com>
2023-05-08 11:32:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,11 @@ if www is not '':
     yolo_weights = 'last_8n_e120.pt'
     
     import yolov8_tracking.track as track
+    with io.BytesIO() as f:
+      pass
+                 #with io.BytesIO() as f:
+                 #im.save(f, format='JPEG')
+                 #data = f.getvalue()
     opt = track.parse_opt(yolo_weights,www)
     #fstr = track.main(opt)
     #st.write(fstr)
